a28814f99d01348771d5670c1bc4b545983b95bc
Arthur Fernandez <arthur@snooget.com>
2023-04-20 09:45:14 -0700

    added getters and setters with classes

--- classes-notes.js
+++ classes-notes.js
@@ -41,7 +41,6 @@ class Surgeon {
         }
     }
     const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
-    
     const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
 // End of lesson 3: Instances
 //============================================================================================================================
@@ -66,5 +65,31 @@ class Surgeon {
         this._behavior++;
         }
     }
+    class Surgeon {
+        constructor(name, department) {
+          this._name = name;
+          this._department = department;
+          this._remainingVacationDays = 20;
+        }
+      
+        get name() {
+          return this._name;
+        }
+      
+        get department(){
+          return this._department;
+        }
+      
+        get remainingVacationDays() {
+          return this._remainingVacationDays;
+        }
+      
+        takeVacationDays(daysOff) {
+          this._remainingVacationDays -= daysOff;
+        }
+      }
+      
+      const surgeonRomero2 = new Surgeon('Francisco Romero', 'Cardiovascular');
+      const surgeonJackson2 = new Surgeon('Ruth Jackson', 'Orthopedics');
 
     // End of the example ========================
